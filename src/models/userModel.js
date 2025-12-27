88e70e1a81a872168a65a7c5104e81b01108a1f4
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-12-27 11:20:00 +0600

    add findUserByEmail function to user model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,3 +3,8 @@ let usersCollection;
 const initUsersCollection = (db)=>{
     usersCollection = db.collection('users')
 }
+
+const findUserByEmail = (email)=>{
+    return usersCollection.findOne({email})
+}
+
