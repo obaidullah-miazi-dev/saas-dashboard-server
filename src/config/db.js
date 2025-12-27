3739e532d8d19b2ac18e127b320f01b053073f48
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-12-27 11:14:25 +0600

    added db.js file for connect to mongoDB

---
+++ src/config/db.js
@@ -0,0 +1,19 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = process.env.MONOGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const connectDB = async () => {
+  await client.connect();
+  console.log("Connected to MongoDB!");
+  return client.db("Saas-DB");
+};
+
+module.exports = connectDB;
